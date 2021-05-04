f9dd7033bf072a078f603bc92cfb3b615fcdba63
Sydney Jodon <sydney.jodon@workiva.com>
2021-05-04 14:12:22 -0700

    Add test for user-event type with selection range

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import "@testing-library/jest-dom/extend-expect";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("delete characters within the selectedRange", () => {
+    debugger;
+    render(
+        <div>
+            <label htmlFor="my-input">Example:</label>
+            <input id="my-input" type="text" defaultValue="This is a bad example" />
+        </div>
+    );
+    const input = screen.getByLabelText(/example/i);
+    debugger;
+    input.setSelectionRange(10, 13);
+    userEvent.type(input, "good");
+
+    expect(input).toHaveValue("This is a good example");
 });
